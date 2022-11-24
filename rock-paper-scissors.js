0e2a72e474bcf59dd15edd6d17bdc4736053752c
Micheal Olanegan <mikkysar@gmail.com>
2022-11-24 21:54:55 +0000

    completed html-css

---
+++ rock-paper-scissors.js
@@ -0,0 +1,11 @@
+/*const test = prompt("hey");*/
+
+function play(){
+    const images = document.querySelectorAll('score-images');
+    let rock_image = 
+    
+    console.log(imageDir)
+
+
+}
+play()
